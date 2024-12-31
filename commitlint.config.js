ed0b6ce6ecabf2b0ff9002731818006986b0a167
Bethany Fannin <contact@bethanyfannin.com>
2024-12-31 18:03:22 -0500

    ci(commitlint/commitizen): update Commitlint and Commitizen types
    Add 'workflow' type for Github specific actions and issue/PR templates

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+const CommitlintConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'revert',
+        'build',
+        'ci',
+        'chore',
+        'release',
+        'workflow',
+      ],
+    ],
+  }
+}
