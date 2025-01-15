e2ed572ec4e8d88eb7f26deff769d56e0c4f3627
Bethany Fannin <bethany@bethanyfannin.com>
2025-01-15 12:56:03 -0500

    ci: finish local CI setup and testing/formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import jsLint from '@eslint/js';
 import typescriptLint from 'typescript-eslint';
-import jestPlugin from 'eslint-plugin-jest';
+import vitestPlugin from '@vitest/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 
@@ -10,6 +10,7 @@ export default typescriptLint.config(
   typescriptLint.configs.recommendedTypeChecked,
   eslintConfigPrettier,
   {
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts, **/*.mts'],
     languageOptions: {
       globals: {
         ...globals.node,
@@ -25,31 +26,26 @@ export default typescriptLint.config(
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars-experimental': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
   {
     files: ['**/*.{js,cjs,mjs}'],
+    ignores: ['.husky', 'node_modules', '.git', 'coverage'],
     extends: [typescriptLint.configs.disableTypeChecked],
   },
   {
     files: ['**/*.spec.ts', '**/*.test.ts'],
-    plugins: {
-      jest: jestPlugin,
-    },
-    settings: {
-      jest: {
-        version: 27,
-      },
+    ...vitestPlugin.configs.recommended,
+    languageOptions: {
       globals: {
-        jest: true,
-        describe: true,
-        test: true,
-        expect: true,
-        beforeEach: true,
-        afterEach: true,
-        beforeAll: true,
-        afterAll: true,
+        ...vitestPlugin.environments.env.globals,
       },
     },
+  },
+  {
+    ignores: ['dist', '.husky', 'node_modules', '.git', 'coverage', '*.env*'],
   }
 );
